fd60e2917c30dcdcbde6e681fce2818392aa5638
papydur <joostvdp92@gmail.com>
2021-08-07 07:40:51 +0200

    Added three projects to the projects page

--- static/script_homepage.js
+++ static/script_homepage.js
@@ -27,12 +27,28 @@ function setTheme(mode){
     localStorage.setItem('theme', mode)
 }
 
-$("a[href='#top']").click(function() {
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+$("#myBtn").click(function() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 });
 
-$('.content-wrapper').load('pages/contact.html')
+
+$('.content-wrapper').load('pages/projects.html')
 
 $("#id-contact").click(function(){
     $(".content-wrapper").slideUp(function () {
